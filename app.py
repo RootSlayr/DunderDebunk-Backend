b85de48bed6c6c94bf07260180f696775ad5b69a
Anirudh Chidambaram <anirudh.chida@gmail.com>
2025-04-03 22:26:31 +1100

    Added courses and course data in the backedn and sending them to frontent

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from courses import courses  # Import the courses list from the courses.py file
 
 app = Flask(__name__)
 CORS(app)
@@ -7,7 +8,18 @@ CORS(app)
 
 @app.route("/api/test", methods=["GET"])
 def test():
+    """Testing 1 2 3"""
     return jsonify(message="Testing Backend")
+
+
+@app.route("/courses", methods=["GET"])
+def get_courses():
+    """
+    Calls the courses Dictionary and returns it as a JSON object.
+    This is used to display the courses on the frontend.
+    The courses are stored in a list of dictionaries, where each dictionary contains the course id, name, and image path.
+    """
+    return jsonify(courses)
 
 
 if __name__ == "__main__":
---
+++ courses.py
@@ -0,0 +1,20 @@
+courses = [
+    {
+        "id": 1,
+        "name": "Fact-Checking & Source Verification",
+        "image": r"fact-check.jpg",
+        "desc": "Learn how to verify the authenticity of sources and information.",
+    },
+    {
+        "id": 2,
+        "name": "Recognizing Manipulation Tactics",
+        "image": r"manipulation.jpg",
+        "desc": "Understand the tactics used to manipulate information and narratives.",
+    },
+    {
+        "id": 3,
+        "name": "Understanding Misinformation",
+        "image": "misinformation.jpg",
+        "desc": "Explore the different types of misinformation and their impact.",
+    },
+]
